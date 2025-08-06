5b1e201c3cd697351f248951240ff9ef35ba8169
NicoleBertolino <nicole_b_l_santos@hotmail.com>
2025-08-06 10:57:26 -0300

    corrections to the backtracking method

--- src/backtracking.c
+++ src/backtracking.c
@@ -4,7 +4,7 @@
 
 int *melhor_solucao = NULL;
 int *solucao_atual = NULL;
-
+int maior_lucro = 0;
 // Retorna o maior entre dois inteiros
 int maximo(int a, int b) {
     return (a > b) ? a : b;
@@ -12,7 +12,7 @@ int maximo(int a, int b) {
 
 // Função recursiva de backtracking
 void mochila_backtracking(Item *itens, int indice_atual, int total_itens, int capacidade_mochila,
-                          int lucro_atual, int peso_atual, int maior_lucro) {
+                          int lucro_atual, int peso_atual) {
 
     if (indice_atual == total_itens) {
         // Verifica se a solução atual é melhor do que a melhor encontrada até agora
@@ -30,18 +30,18 @@ void mochila_backtracking(Item *itens, int indice_atual, int total_itens, int ca
 
         mochila_backtracking(itens, indice_atual + 1, total_itens, capacidade_mochila,
                              lucro_atual + itens[indice_atual].value,
-                             peso_atual + itens[indice_atual].weigh, maior_lucro);
+                             peso_atual + itens[indice_atual].weigh);
 
         solucao_atual[indice_atual] = 0; // desfaz a inclusão para testar outros caminhos
     }
 
     // Tenta excluir o item atual
     mochila_backtracking(itens, indice_atual + 1, total_itens, capacidade_mochila,
-                         lucro_atual, peso_atual, maior_lucro);
+                         lucro_atual, peso_atual);
 }
 
 // Exibe os resultados da melhor solução encontrada
-void exibirResultadoBacktracking(Item *itens, int total_itens, int capacidade_mochila, int maior_lucro) {
+void exibirResultadoBacktracking(Item *itens, int total_itens, int capacidade_mochila) {
     printf("Lucro máximo obtido: R$ %d\n", maior_lucro);
     printf("Itens selecionados:\n");
 
@@ -70,11 +70,11 @@ int executarBacktracking(Backpack *mochila) {
     if (melhor_solucao == NULL || solucao_atual == NULL)
         return -1;
 
-   int maior_lucro = 0;
+   maior_lucro = 0;
 
-    mochila_backtracking(itens, 0, total_itens, capacidade_mochila, 0, 0, maior_lucro);
+    mochila_backtracking(itens, 0, total_itens, capacidade_mochila, 0, 0);
 
-    exibirResultadoBacktracking(itens, total_itens, capacidade_mochila, maior_lucro);
+    exibirResultadoBacktracking(itens, total_itens, capacidade_mochila);
 
     free(melhor_solucao);
     free(solucao_atual);
